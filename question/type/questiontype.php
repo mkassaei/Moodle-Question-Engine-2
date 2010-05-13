1160feb1d438552823d201004c08c792b023f6da
Tim Hunt <T.J.Hunt@open.ac.uk>
2010-05-13 10:32:45 +0100

    Fix notices when importing questions.

--- question/type/questiontype.php
+++ question/type/questiontype.php
@@ -395,10 +395,23 @@ class question_type {
     public function save_hints($formdata, $withparts = false) {
         delete_records('question_hints', 'questionid', $formdata->id);
 
-        $numhints = max(array_keys($formdata->hint)) + 1;
+        if (!empty($formdata->hint)) {
+            $numhints = max(array_keys($formdata->hint)) + 1;
+        } else {
+            $numhints = 0;
+        }
+
         if ($withparts) {
-            $numclears = max(array_keys($formdata->hintclearwrong)) + 1;
-            $numshows = max(array_keys($formdata->hintshownumcorrect)) + 1;
+            if (!empty($formdata->hintclearwrong)) {
+                $numclears = max(array_keys($formdata->hintclearwrong)) + 1;
+            } else {
+                $numclears = 0;
+            }
+            if (!empty($formdata->hintshownumcorrect)) {
+                $numshows = max(array_keys($formdata->hintshownumcorrect)) + 1;
+            } else {
+                $numshows = 0;
+            }
             $numhints = max($numhints, $numclears, $numshows);
         }
 
