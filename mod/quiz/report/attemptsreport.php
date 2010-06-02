fe86b0d77f024b57ae4e5cde3d3e1119ed13c615
Tim Hunt <T.J.Hunt@open.ac.uk>
2010-06-02 18:34:19 +0100

    Notice from the quiz reports when a group is selected.

--- mod/quiz/report/attemptsreport.php
+++ mod/quiz/report/attemptsreport.php
@@ -92,7 +92,7 @@ abstract class quiz_attempt_report extends quiz_default_report {
         // We have a currently selected group.
         if (!$groupstudents = get_users_by_capability($this->context,
                 array('mod/quiz:reviewmyattempts', 'mod/quiz:attempt'),
-                'id,1', '', '', '', $currentgroup, '', false)) {
+                'u.id,1', '', '', '', $currentgroup, '', false)) {
             $groupstudents = array();
         } else {
             $groupstudents = array_keys($groupstudents);
